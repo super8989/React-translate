a2356ea5a742ef809f4a7de078d1b7a22a89eb06
Sam <super8989@gmail.com>
2019-04-02 22:09:53 -0400

    the context provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends React.Component {
   state = { language: "english" };
@@ -16,7 +17,9 @@ class App extends React.Component {
           <i className="flag ca" onClick={() => this.onLanguageChange('english')} />
           <i className="flag fr" onClick={() => this.onLanguageChange('french')} />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }

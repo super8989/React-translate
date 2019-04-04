bb842383e4d4275e7baa1fc0b6353c937de173d3
Sam <super8989@gmail.com>
2019-04-03 22:36:30 -0400

    pulling from multiple contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,37 @@
 import React from "react";
-import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
 
   onLanguageChange = language => {
-    this.setState({ language }); {/* language: language*/}
-  }
+    this.setState({ language });
+    {
+      /* language: language*/
+    }
+  };
 
   render() {
     return (
       <div className="ui container">
         <div>
           Select a language:
-          <i className="flag ca" onClick={() => this.onLanguageChange('english')} />
-          <i className="flag fr" onClick={() => this.onLanguageChange('french')} />
+          <i
+            className="flag ca"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag fr"
+            onClick={() => this.onLanguageChange("french")}
+          />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
